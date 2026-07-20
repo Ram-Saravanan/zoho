19e7ed901dbec0f6f2bd3060b3390cf5ba9b68fb
Ram-Saravanan <ramsaravananfr@gmail.com>
2026-07-20 19:25:58 +0530

    x

--- day1/odd asc even descending.py
+++ day1/odd asc even descending.py
@@ -1,13 +1,30 @@
 a = [1,2,3,4,5,6,7,8,9]
 
-l = 0
+odd = []
+even = []
 
-for r in range(len(a)):
-    if a[r]%2!=0:
-        a[l],a[r] = a[r],a[l]
-        l += 1
+for i in a:
+    if i%2 == 0:
+        even.append(i)
+    else:
+        odd.append(i)
 
-odd = sorted(a[:l])
-even = sorted(a[l:], reverse= True)
+odd.sort()
+even.sort(reverse=True)
 
-print(odd + even)
+print (odd + even)
+
+
+## two pointer
+# a = [1,2,3,4,5,6,7,8,9]
+# l = 0
+
+# for r in range(len(a)):
+#     if a[r]%2!=0:
+#         a[l],a[r] = a[r],a[l]
+#         l += 1
+
+# odd = sorted(a[:l])
+# even = sorted(a[l:], reverse= True)
+
+# print(odd + even)
--- day1/remove_dup.py
+++ day1/remove_dup.py
@@ -5,5 +5,3 @@ for i in num:
     if i not in arr:
         arr.append(i)
 print(arr)
-
-
